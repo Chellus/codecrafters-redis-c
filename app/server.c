c8f30398418212a32dd7e288814b48a3260cd43e
Chellus <elias4845@gmail.com>
2024-11-11 11:12:54 -0300

    concurrent clients

--- app/server.c
+++ app/server.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <sys/socket.h>
+#include <sys/select.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
+
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
@@ -24,8 +27,17 @@ int main()
 	printf("Logs from your program will appear here!\n");
 
 
-	int server_fd, client_addr_len;
+	int server_fd, client_addr_len, new_fd, client_fds[30], max_clients = 30;
+	int activity, valread, i, sd;
+	int max_fd;
+
+	fd_set readfds;
 	
+	// init all client_socket[] to 0
+	for (i = 0; i < max_clients; i++)
+		client_fds[i] = 0;
+
+
 	server_fd = init_server_socket();
 	if (server_fd == -1)
 		return 1;
@@ -36,9 +48,57 @@ int main()
 	printf("Waiting for a client to connect...\n");
 	client_addr_len = sizeof(client_addr);
 
-	int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+	//int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+	//handle_client_connection(client_fd);
 
-	handle_client_connection(client_fd);
+	while (1) {
+		// clear the fd_set
+		FD_ZERO(&readfds);
+
+		// add server socket to set
+		FD_SET(server_fd, &readfds);
+		max_fd = server_fd;
+
+		// add client sockets to set
+		for (i = 0; i < 30; i++) {
+			// if this is a valid connection
+			if (client_fds[i] > 0)
+				FD_SET(client_fds[i], &readfds);
+
+			// to calculate nfds
+			if (client_fds[i] > max_fd)
+				max_fd = client_fds[i];
+		}
+
+		// wait for an activity on one of the sockets
+		activity = select(max_fd + 1, &readfds, NULL, NULL, NULL);
+		if ((activity < 0) && (errno != EINTR)) {
+			printf("Select error: %s...\n", strerror(errno));
+			return 1;
+		}
+
+		// if something happened on the master socket, its an incoming connection
+		if (FD_ISSET(server_fd, &readfds)) {
+			int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+			
+			handle_client_connection(client_fd);
+
+			for (i = 0; i < max_clients; i++) {
+				if (client_fds[i] == 0) {
+					client_fds[i] = client_fd;
+					break;
+				}
+			}
+		}
+
+		// else its some IO operation in some client socket
+		for (i = 0; i < 30; i++) {
+			if (FD_ISSET(client_fds[i], &readfds)) {
+				handle_client_connection(client_fds[i]);
+			}
+		}
+
+	}
 
 	close(server_fd);
 
