b437d3bff7f1b8a20d77e15d1553e99095d650ff
Chellus <elias4845@gmail.com>
2024-11-28 19:49:27 -0300

    expiry...

--- app/time/getmillis.h
+++ app/time/getmillis.h
@@ -8,7 +8,7 @@ long currentMillis() {
     struct timeval tp;
 
     gettimeofday(&tp, NULL);
-    return tp.tv_sec * 1000 + tp.tv_usec / 1000;
+    return tp.tv_sec * 1000 + tp.tv_usec / 100000;
 }
 
 #endif
